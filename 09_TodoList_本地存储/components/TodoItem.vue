a7aa2cbba25c52e11d46592c188cf8c0d09f77ad
hosealle <erikssonhou@gmail.com>
2023-04-20 16:50:31 +0800

    修改TodoList项目为本地存储数据

---
+++ 09_TodoList_本地存储/components/TodoItem.vue
@@ -0,0 +1,75 @@
+<template>
+    <li>
+        <label>
+            <input type="checkbox" :checked="todo.completed" @change="handleCheck(todo.id)" />
+            <span>{{ todo.content }}</span>
+        </label>
+        <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+    </li>
+</template>
+<script>
+export default {
+    name: 'TodoItem',
+    props: ['todo', 'checkTodoItem', 'deleteTodoItem'],
+    methods: {
+        // 勾选 or 取消勾选
+        handleCheck(id) {
+            // 通知App组件将对应的todo对象的done值取反
+            this.checkTodoItem(id)
+        },
+        // 删除
+        handleDelete(id){
+            // confirm函数如果点击确定就返回true，反之返回false
+            if(confirm('确定删除吗？')){
+                // 通知App删除对应id值的todoItem
+                this.deleteTodoItem(id)
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+/*item*/
+li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+}
+
+li label {
+    float: left;
+    cursor: pointer;
+}
+
+li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+}
+
+li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+}
+
+li:before {
+    content: initial;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+
+li:hover{
+    background-color: #ddd;
+}
+
+li:hover button{
+    display: inline;
+}
+
+</style>
---
+++ 09_TodoList_本地存储/components/TodoListFooter.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="todo-footer" v-show="totalTodoNumber">
+        <label>
+            <!-- <input type="checkbox" :checked="isAll" @change="checkAll" ref="itemCheckBox" /> -->
+            <input type="checkbox" v-model="isAll" ref="itemCheckBox" />
+        </label>
+        <span>
+            <span>已完成{{ completedTodoNumber }}</span> / 全部{{ totalTodoNumber }}
+        </span>
+        <button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TodoListFooter',
+    props: ['todos', 'checkAllTodoItem', 'clearAllTodoItem'],
+    computed: {
+        isAll: {
+            get() {
+                return this.totalTodoNumber === this.completedTodoNumber && this.totalTodoNumber > 0
+            },
+            set(val){
+                this.checkAllTodoItem(val)
+            }
+        },
+        totalTodoNumber() {
+            return this.todos.length
+        },
+        completedTodoNumber() {
+            // 法1
+            // return this.todos.filter(todo => todo.completed == true).length
+            // 法2：用reduce实现条件统计，注意括号！
+            return this.todos.reduce((pre, curr) => (pre + (curr.completed ? 1 : 0)), 0)
+        }
+    },
+    methods: {
+        clearAll(){
+            this.clearAllTodoItem()
+        }
+    }
+}
+</script>
+<style scoped>
+/*footer*/
+.todo-footer {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+}
+
+.todo-footer label {
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+}
+
+.todo-footer label input {
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+}
+
+.todo-footer button {
+    float: right;
+    margin-top: 5px;
+}
+</style>
