f3667edf077ca6248279cc50ff2f55cbf3a85e3e
hosealle <erikssonhou@gmail.com>
2023-04-28 16:11:36 +0800

    事件总线

---
+++ src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="school">
+    <h2>学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+  </div>
+</template>
+<script>
+import {HELLO} from '@/config/constants'
+export default {
+  name: "SchoolItem",
+  data() {
+    return {
+      name: "USTB",
+      address: "beijing",
+    }
+  },
+  mounted() {
+    this.$bus.$on(HELLO, (data) => {
+      console.log("我是School组件，收到了数据", data)
+    })
+  },
+  beforeDestroy(){
+    this.$bus.$off(HELLO)
+  }
+}
+</script>
+<style scoped>
+.school {
+  background-color: skyblue;
+  padding: 5px;
+}
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="student">
+    <h2>学生名称: {{ name }}</h2>
+    <h2>学生性别: {{ gender }}</h2>
+    <button @click="sendStudentName">把学生名给School组件</button>
+  </div>
+</template>
+<script>
+import {HELLO} from '@/config/constants'
+export default {
+  name: "StudentItem",
+  data() {
+    return {
+      name: "Rose",
+      gender: "female",
+    }
+  },
+  methods: {
+    sendStudentName() {
+      this.$bus.$emit(HELLO, 123)
+    },
+  },
+}
+</script>
+<style scoped>
+.student {
+  background-color: orange;
+  padding: 5px;
+}
+</style>
