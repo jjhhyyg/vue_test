d892e9c600441eb2737415a69c59762aff769446
hosealle <erikssonhou@gmail.com>
2023-04-29 14:12:57 +0800

    none

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -14,7 +14,7 @@ export default {
   },
   methods:{
     getInfo(){
-      axios.get("http://localhost:8080/users/").then(
+      axios.get("http://localhost:8080/hhh/users").then(
         response => {
           console.log(response.data)
         },
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, //关闭语法检查
   devServer: {
-    proxy: 'http://localhost:8082'
-  }
+    proxy: {
+      "/hhh": {
+        target: "http://localhost:8082",
+        ws: true, // 默认为true，打开websocket
+        changeOrigin: false, // 默认为true，用于控制请求头中的host值；如果为false，则会把请求的实际发送端口给后端，否则将告诉后端端口与它一致
+        // 让后端服务器收到的url是localhost:8082/users而不是localhost:8082/hhh/users
+        pathRewrite: {
+          "^/hhh": "",
+        },
+      },
+    },
+  },
 })
